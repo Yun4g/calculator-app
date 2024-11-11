fd2fc203ced766334ed4780393a999061147d1bb
Yun4g <delightvincent487@gmail.com>
2024-11-11 21:22:44 +0100

    commit

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
 const display = document.querySelector('#display');
 let result  = document.querySelector(".result");
+let buttons = document.querySelectorAll('button');
 
-function output(input){
-    display.value += input;
 
+for (let i = 0; i < buttons.length; i++) {
+      
+    buttons[i].addEventListener('click', ()=>{
+        let buttonText = buttons[i].textContent;
+        if (buttonText !== ' = ' && buttonText !== 'AC'  ) {
+        display.value += buttonText;
+        console.log(display.value);
+        }
+    })
+    
 }
 
 function remove(){
     display.value = "";
-    result.textContent = " ";
-
+     result.textContent = " ";
 }
 
 function calculate(){
-     let evaluate =  eval(display.value);
-     result.textContent = evaluate
-     console.log(result)
-}
+          let evaluate =  eval(display.value);
+          result.textContent = evaluate
+          console.log(result)
+     }
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function output(input){
+//     display.value += input;
+
+// }
+
+// function remove(){
+//     display.value = "";
+//     result.textContent = " ";
+
+// }
+
+// function calculate(){
+//      let evaluate =  eval(display.value);
+//      result.textContent = evaluate
+//      console.log(result)
+// }
 
